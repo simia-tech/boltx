4de3160beb16f4243eeb8fe63b5b71acf14332e9
Philipp Brüll <bruell@simiatech.com>
2016-08-05 09:23:26 +0200

    refactored cursor again

--- cursor.go
+++ cursor.go
@@ -3,104 +3,26 @@ package boltx
 import (
 	"encoding"
 
-	"github.com/boltdb/bolt"
 	"github.com/pkg/errors"
 )
 
-// Cursorx implements an extended cursor that unmarshals the selected values.
-type Cursorx struct {
-	bucket *bolt.Bucket
-	cursor *bolt.Cursor
-}
+// CursorWrapper returns a function that can be used to wrap calls to bolt.Cursor.
+//
+//   model := &model{}
+//   cursor, wrapper := bucket.Cursor(), boltx.CursorWrapper(model)
+//   for key, err := wrapper(cursor.First()); key != nil && err == nil; key, err = wrapper(cursor.Next()) {
+//     fmt.Println(model.field)
+//   }
+func CursorWrapper(model encoding.BinaryUnmarshaler) func([]byte, []byte) ([]byte, error) {
+	return func(key, value []byte) ([]byte, error) {
+		if key == nil {
+			return nil, nil
+		}
 
-// Cursor returns an extended cursor on the provided bucket.
-func Cursor(bucket *bolt.Bucket) *Cursorx {
-	return &Cursorx{
-		bucket: bucket,
-		cursor: bucket.Cursor(),
+		if err := model.UnmarshalBinary(value); err != nil {
+			return key, errors.Wrap(err, "unmarshaling failed")
+		}
+
+		return key, nil
 	}
-}
-
-// Bucket returns the bucket that this cursor was created from.
-func (c *Cursorx) Bucket() *bolt.Bucket {
-	return c.bucket
-}
-
-// Delete removes the key/value under the cursor.
-func (c *Cursorx) Delete() error {
-	if err := c.cursor.Delete(); err != nil {
-		return err
-	}
-	return nil
-}
-
-// First moves the cursor to the first element and unmarshals the value into the provided model.
-func (c *Cursorx) First(model encoding.BinaryUnmarshaler) ([]byte, error) {
-	key, value := c.cursor.First()
-	if key == nil {
-		return nil, nil
-	}
-
-	if err := model.UnmarshalBinary(value); err != nil {
-		return key, errors.Wrap(err, "unmarshaling failed")
-	}
-
-	return key, nil
-}
-
-// Last moves the cursor to the last element and unmarshals the value into the provided model.
-func (c *Cursorx) Last(model encoding.BinaryUnmarshaler) ([]byte, error) {
-	key, value := c.cursor.Last()
-	if key == nil {
-		return nil, nil
-	}
-
-	if err := model.UnmarshalBinary(value); err != nil {
-		return key, errors.Wrap(err, "unmarshaling failed")
-	}
-
-	return key, nil
-}
-
-// Next moves the cursor to the next element and unmarshals the value into the provided model.
-func (c *Cursorx) Next(model encoding.BinaryUnmarshaler) ([]byte, error) {
-	key, value := c.cursor.Next()
-	if key == nil {
-		return nil, nil
-	}
-
-	if err := model.UnmarshalBinary(value); err != nil {
-		return key, errors.Wrap(err, "unmarshaling failed")
-	}
-
-	return key, nil
-}
-
-// Prev moves the cursor to the previous element and unmarshals the value into the provided model.
-func (c *Cursorx) Prev(model encoding.BinaryUnmarshaler) ([]byte, error) {
-	key, value := c.cursor.Prev()
-	if key == nil {
-		return nil, nil
-	}
-
-	if err := model.UnmarshalBinary(value); err != nil {
-		return key, errors.Wrap(err, "unmarshaling failed")
-	}
-
-	return key, nil
-}
-
-// Seek moves the cursor to the provided key and returns it. If the key does not exists, the next
-// key is used. The corresponding value is unmarshaled into the provided model.
-func (c *Cursorx) Seek(prefix []byte, model encoding.BinaryUnmarshaler) ([]byte, error) {
-	key, value := c.cursor.Seek(prefix)
-	if key == nil {
-		return nil, nil
-	}
-
-	if err := model.UnmarshalBinary(value); err != nil {
-		return key, errors.Wrap(err, "unmarshaling failed")
-	}
-
-	return key, nil
 }
--- cursor_test.go
+++ cursor_test.go
@@ -10,39 +10,17 @@ import (
 	"github.com/simia-tech/boltx"
 )
 
-func TestCursorIteratingForward(t *testing.T) {
+func TestCursorIterating(t *testing.T) {
 	db, tearDown := setUpTestDB(t)
 	defer tearDown()
 
 	inTestBucket(t, db, func(bucket *bolt.Bucket) {
 		require.NoError(t, boltx.Put(bucket, []byte("test"), &model{field: "test"}))
 
-		cursor := boltx.Cursor(bucket)
-		assert.Equal(t, bucket, cursor.Bucket())
-
 		count := 0
 		model := &model{}
-		for key, err := cursor.First(model); key != nil && err == nil; key, err = cursor.Next(model) {
-			assert.Equal(t, "test", model.field)
-			count++
-		}
-		assert.Equal(t, 1, count)
-	})
-}
-
-func TestCursorIteratingBackward(t *testing.T) {
-	db, tearDown := setUpTestDB(t)
-	defer tearDown()
-
-	inTestBucket(t, db, func(bucket *bolt.Bucket) {
-		require.NoError(t, boltx.Put(bucket, []byte("test"), &model{field: "test"}))
-
-		cursor := boltx.Cursor(bucket)
-		assert.Equal(t, bucket, cursor.Bucket())
-
-		count := 0
-		model := &model{}
-		for key, err := cursor.Last(model); key != nil && err == nil; key, err = cursor.Prev(model) {
+		cursor, wrapper := bucket.Cursor(), boltx.CursorWrapper(model)
+		for key, err := wrapper(cursor.First()); key != nil && err == nil; key, err = wrapper(cursor.Next()) {
 			assert.Equal(t, "test", model.field)
 			count++
 		}
@@ -54,20 +32,12 @@ func TestCursorIteratingInEmptyBucket(t *testing.T) {
 	db, tearDown := setUpTestDB(t)
 	defer tearDown()
 
-	assertNoElement := func(key []byte, err error) {
-		assert.Nil(t, key)
-		require.NoError(t, err)
-	}
-
 	inTestBucket(t, db, func(bucket *bolt.Bucket) {
-		cursor := boltx.Cursor(bucket)
-		assert.Equal(t, bucket, cursor.Bucket())
+		cursor, wrapper := bucket.Cursor(), boltx.CursorWrapper(nil)
 
-		assertNoElement(cursor.First(nil))
-		assertNoElement(cursor.Last(nil))
-		assertNoElement(cursor.Next(nil))
-		assertNoElement(cursor.Prev(nil))
-		assertNoElement(cursor.Seek(nil, nil))
+		key, err := wrapper(cursor.First())
+		require.NoError(t, err)
+		assert.Nil(t, key)
 	})
 }
 
@@ -75,47 +45,13 @@ func TestCursorIteratingOverInvalidElements(t *testing.T) {
 	db, tearDown := setUpTestDB(t)
 	defer tearDown()
 
-	assertError := func(key []byte, err error) {
-		assert.NotNil(t, key)
-		assert.Error(t, err)
-	}
-
 	inTestBucket(t, db, func(bucket *bolt.Bucket) {
-		require.NoError(t, bucket.Put([]byte("one"), []byte("invalid")))
-		require.NoError(t, bucket.Put([]byte("two"), []byte("invalid")))
-
-		cursor := boltx.Cursor(bucket)
-		assert.Equal(t, bucket, cursor.Bucket())
+		require.NoError(t, bucket.Put([]byte("test"), []byte("invalid")))
 
 		model := &model{}
-		assertError(cursor.First(model))
-		assertError(cursor.Next(model))
-		assertError(cursor.Last(model))
-		assertError(cursor.Prev(model))
-		assertError(cursor.Seek(nil, model))
-	})
-}
-
-func TestCursorSeekAndDelete(t *testing.T) {
-	db, tearDown := setUpTestDB(t)
-	defer tearDown()
-
-	inTestBucket(t, db, func(bucket *bolt.Bucket) {
-		require.NoError(t, boltx.Put(bucket, []byte("test"), &model{field: "test"}))
-
-		cursor := boltx.Cursor(bucket)
-		assert.Equal(t, bucket, cursor.Bucket())
-
-		model := &model{}
-		key, err := cursor.Seek([]byte("t"), model)
-		require.NoError(t, err)
+		cursor, wrapper := bucket.Cursor(), boltx.CursorWrapper(model)
+		key, err := wrapper(cursor.First())
 		assert.Equal(t, "test", string(key))
-		assert.Equal(t, "test", model.field)
-
-		require.NoError(t, cursor.Delete())
-
-		found, err := boltx.Get(bucket, []byte("test"), model)
-		require.NoError(t, err)
-		assert.False(t, found)
+		assert.Error(t, err)
 	})
 }
